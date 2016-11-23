9f926ec2a46c2a69483cf27a840eafa98d18f891
Jobin Johnson <jobinrjohnson@gmail.com>
2016-11-23 08:26:46 -0500

    Update jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -2,3 +2,6 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
+echo "\nUse the below password for Jenkins for its initial setup\n==============================================\n"
+cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "\n==============================================\n"
