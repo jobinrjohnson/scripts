221b6c480410b2ede7a278a17f6713b03d5b9b03
Jobin Johnson <jobinrjohnson@gmail.com>
2017-03-08 05:52:13 +0530

    Update romsetup.sh

--- romsetup.sh
+++ romsetup.sh
@@ -4,20 +4,25 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+#remove any open jdk instances
+sudo apt-get purge openjdk-\* icedtea-\* icedtea6-\*
+
 #installing essential packages
 apt-get update
-sudo apt-get upgrade
-apt-get install gcc g++ dpkg-dev
-apt-get --assume-yes install bison build-essential curl flex git git-core gnupg gperf libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev libxml2 libxml2-utils lzop openjdk-7-jdk openjdk-7-jre pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev unzip lib32readline-gplv2-dev lib32z1-dev make python-networkx zlib1g-dev:i386 phablet-tools
+apt-get upgrade
+apt-get install openjdk-8-jdk
+apt-get install bison g++-multilib git gperf libxml2-utils make zlib1g-dev:i386 zip
 
 #setting up environments
 mkdir -p ~/bin
-PATH=~/bin:$PATH  
 cd ~/bin
 
 #setting up repo
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo  
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
+
+echo "export PATH=~/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
 
 #configuring git
 read -p "Please enter your name for git: " git_name
