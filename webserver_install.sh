8fb7c407ce60e24cc0a3b205a7f9a6c97cb1cc23
Jobin Johnson <jobinrjohnson@gmail.com>
2017-05-09 05:58:16 +0000

    added comments

--- webserver_install.sh
+++ webserver_install.sh
@@ -1,11 +1,13 @@
-
 #!/bin/bash
+
+#check uid
 if [ "$EUID" -ne 0 ]
         then
         echo "Please run this script as root"
         exit
 fi
 
+#Version checks
 VER=$(lsb_release -sr)
 if [ "$VER" != "14.04" ]
         then
@@ -14,11 +16,20 @@ if [ "$VER" != "14.04" ]
 fi
 
 apt-get update
+
+#install apache
 apt-get --assume-yes install apache2
+#install php
 apt-get --assume-yes install mysql-server php5-mysql
+#configure mysql
 mysql_install_db
+#install php mods
 apt-get --assume-yes install php5 libapache2-mod-php5 php5-mcrypt
+
+#configure directory index
 echo "<IfModule mod_dir.c>" > /etc/apache2/mods-enabled/dir.conf
 echo "DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" >> /etc/apache2/mods-enabled/dir.conf
 echo "</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
+
+#restarting apache2
 service apache2 restart
