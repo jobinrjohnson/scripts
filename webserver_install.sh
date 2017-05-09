daabcca780ecc7018120841b67929c7956edaf52
Jobin Johnson <jobinrjohnson@gmail.com>
2017-05-09 06:04:16 +0000

    fixed directory indexing issue

--- webserver_install.sh
+++ webserver_install.sh
@@ -28,7 +28,7 @@ apt-get --assume-yes install php5 libapache2-mod-php5 php5-mcrypt
 
 #configure directory index
 echo "<IfModule mod_dir.c>" > /etc/apache2/mods-enabled/dir.conf
-echo "DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" >> /etc/apache2/mods-enabled/dir.conf
+echo "        DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm" >> /etc/apache2/mods-enabled/dir.conf
 echo "</IfModule>" >> /etc/apache2/mods-enabled/dir.conf
 
 #restarting apache2
